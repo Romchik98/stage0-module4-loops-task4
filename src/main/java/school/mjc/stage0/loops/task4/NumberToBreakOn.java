d19b0a851a0d1caf96cbda73c6501f12138fad68
Romchik98 <38857653+Romchik98@users.noreply.github.com>
2022-09-07 11:30:10 +0300

    Fix NumberToBreakOn.java

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -3,14 +3,13 @@ package school.mjc.stage0.loops.task4;
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         if (toBreakWith > numberToGoUntil) {
-            System.out.print("iterating till the end");
-        } else {
-            for (int i = 0; i < numberToGoUntil; i++) {
-                if (i == toBreakWith) {
-                    break;
-                }
-                System.out.println(i + 1);
+            System.out.println("iterating till the end");
+        }
+        for (int i = 0; i < numberToGoUntil; i++) {
+            if (i == toBreakWith) {
+                break;
             }
+            System.out.println(i + 1);
         }
     }
 }
